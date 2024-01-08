fbe73ed648925b8fd5205994ae901d16280c4593
fadilamanosi <fadilamanosi@gmail.com>
2024-01-08 15:13:57 +0100

    update

---
+++ laravel/app/Http/Controllers/Recordings.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class Recordings extends Controller
+{
+    public function __invoke()
+    {
+        return response()->json([
+            [
+                "placeholder"   => "",
+                "views"         => 200,
+                "size"          => "200 KB",
+                "last_modified" => "3 minutes ago",
+                "title"         => "Getting it right the first time",
+                "description"   => "",
+                "src"           => '/images/placeholder.jpg',
+                "length"        => "00=>14",
+            ],
+            [
+                "placeholder"   => "",
+                "views"         => 200,
+                "size"          => "200 KB",
+                "last_modified" => "3 minutes ago",
+                "title"         => "Getting it right the first time",
+                "description"   => "",
+                "src"           => '/images/placeholder2.jpg',
+                "length"        => "00=>14",
+            ],
+            [
+                "placeholder"   => "",
+                "views"         => 200,
+                "size"          => "200 KB",
+                "last_modified" => "3 minutes ago",
+                "title"         => "Getting it right the first time",
+                "description"   => "",
+                "src"           => '/images/placeholder3.jpg',
+                "length"        => "00=>14",
+            ],
+            [
+                "placeholder"   => "",
+                "views"         => 200,
+                "size"          => "200 KB",
+                "last_modified" => "3 minutes ago",
+                "title"         => "Getting it right the first time",
+                "description"   => "",
+                "src"           => '/images/placeholder4.jpg',
+                "length"        => "00=>14",
+            ],
+            [
+                "placeholder"   => "",
+                "views"         => 200,
+                "size"          => "200 KB",
+                "last_modified" => "3 minutes ago",
+                "title"         => "Getting it right the first time",
+                "description"   => "",
+                "src"           => '/images/placeholder2.jpg',
+                "length"        => "00=>14",
+            ],
+            [
+                "placeholder"   => "",
+                "views"         => 200,
+                "size"          => "200 KB",
+                "last_modified" => "3 minutes ago",
+                "title"         => "Getting it right the first time",
+                "description"   => "",
+                "src"           => '/images/placeholder3.jpg',
+                "length"        => "00=>14",
+            ],
+
+        ]);
+    }
+}
